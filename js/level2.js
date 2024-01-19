a3f7402ce9c0179b2ceafd7a0566ff8b0d07a3a8
Faizan Saleem <156330053+Codeemto@users.noreply.github.com>
2024-01-19 19:18:34 +0000

    Update level2.js

--- js/level2.js
+++ js/level2.js
@@ -93,7 +93,7 @@ function finishQuiz() {
     localStorage.setItem('finalScore', score);
 
     timerElement.style.display = "none";
-    window.location.href = "http://127.0.0.1:5503/finalscores.html";
+    window.location.href = "https://codeemto.github.io/Gamejs.github.io//finalscores.html";
 }
 
 //function to display the question and start the timer
